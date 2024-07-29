9053b3417f7f8e43b169556f80fcd221cab425ec
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-07-28 21:55:34 -0300

    metodo CheckUserAsync en SeedDB

--- Shopping/Shopping/Data/SeedDb.cs
+++ Shopping/Shopping/Data/SeedDb.cs
@@ -1,14 +1,19 @@
 ﻿using Shopping.Data.Entities;
+using Shopping.Enums;
+using Shopping.Helpers;
+using System.Net;
 
 namespace Shopping.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public SeedDb(DataContext context) //inyectamos el context db
+        public SeedDb(DataContext context, IUserHelper userHelper) 
         {
             _context = context;
+            _userHelper = userHelper;
         }
 
         //Este método se encarga de sembrar datos iniciales en tu base de datos utilizando
@@ -18,6 +23,39 @@ namespace Shopping.Data
             await _context.Database.EnsureCreatedAsync(); //crea db y aplica migraciones
             await CheckCategoriesAsync(); //crea las categorias cuando no existen
             await CheckCountriesAsync();   //crea los paises cuando no existen
+            await CheckRoleAsync(); //crea los roles, necesita la interfaz IUserHelper
+            await CheckUserAsync("1010", "Claudio", "Nunez", "clau@yopmail.com", "322 311 4620", "Acasuso 822", UserType.Admin);
+        }
+
+        private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, UserType userType)
+        {
+            User user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    Document = document,
+                    City = _context.Cities.FirstOrDefault(),
+                    UserType = userType,
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+
+            return user;
+        }
+
+        private async Task CheckRoleAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
         }
 
         private async Task CheckCountriesAsync()
